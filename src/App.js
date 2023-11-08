087d2afa0343807198d3728263a50184b450d1dd
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2023-11-08 16:57:51 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/users.json')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.gender}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.domain}</td>
+              <td>{user.ip}</td>
+              <td>{user.university}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
